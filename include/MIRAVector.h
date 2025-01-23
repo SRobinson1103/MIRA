a94343433d69d1fc6e9656842646f91b9a106f19
Sean Robinson <srobi059@odu.edu>
2025-01-23 06:04:30 -0500

    initial commit

---
+++ include/MIRAVector.h
@@ -0,0 +1,21 @@
+﻿#ifndef MIRA_VECTOR_H
+#define MIRA_VECTOR_H
+
+namespace MIRA
+{
+    struct Vector3
+    {
+        float x, y, z;
+        Vector3 operator+(const Vector3& other) const
+        {
+            return { x + other.x, y + other.y, z + other.z };
+        }
+
+        float Dot(const Vector3& other) const
+        {
+            return x * other.x + y * other.y + z * other.z;
+        }
+    };
+}
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+// main.cpp : Defines the entry point for the application.
+//
+
+int main()
+{
+	return 0;
+}
